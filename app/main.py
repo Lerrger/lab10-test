b98f830985c5612cad32367d6526d452d8f6dd20
Lerrger <gershenzon03@mail.ru>
2026-06-28 19:37:26 +0300

    force fail coverage

--- app/main.py
+++ app/main.py
@@ -27,15 +27,33 @@ def get_sales_report(month: str = Query(..., regex=r"^\d{2}$"), year: str = Quer
         "count": count
     }
 
-def test_1(): return 1
-def test_2(): return 2
-def test_3(): return 3
-def test_4(): return 4
-def test_5(): return 5
-def test_6(): return 6
-def test_7(): return 7
-def test_8(): return 8
-def test_9(): return 9
-def test_10(): return 10
-def test_11(): return 11
-def test_12(): return 12
+def junk_1(): return 1
+def junk_2(): return 2
+def junk_3(): return 3
+def junk_4(): return 4
+def junk_5(): return 5
+def junk_6(): return 6
+def junk_7(): return 7
+def junk_8(): return 8
+def junk_9(): return 9
+def junk_10(): return 10
+def junk_11(): return 11
+def junk_12(): return 12
+def junk_13(): return 13
+def junk_14(): return 14
+def junk_15(): return 15
+def junk_16(): return 16
+def junk_17(): return 17
+def junk_18(): return 18
+def junk_19(): return 19
+def junk_20(): return 20
+def junk_21(): return 21
+def junk_22(): return 22
+def junk_23(): return 23
+def junk_24(): return 24
+def junk_25(): return 25
+def junk_26(): return 26
+def junk_27(): return 27
+def junk_28(): return 28
+def junk_29(): return 29
+def junk_30(): return 30
